aa651b6dd9673e87841537791049b3803196597d
zhanglei-amd <lzhang24@amd.com>
2025-06-24 10:24:10 +0800

    bootstrap&harbor (#31)

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-HARBORPWD=$1
-DOMAIN=$2
-CLUSTER=cluster-test
-STORAGE=rook-ceph
 KUBE_CONFIG=/etc/kubernetes/admin.conf
 AUTHORIZE=${HOME}/.ssh/id_rsa
 
@@ -14,6 +10,7 @@ CONFIG_FILE=../hosts.yaml
 NODE_LOCAL_DNS_IP=169.254.25.10
 KUBE_PODS_SUBNET=172.16.0.0/12 #10.232.0.0/14
 KUBE_SERVICE_ADDRESSES=192.168.0.0/16 #10.236.0.0/14
+KUBEENETWORKPLUGIN=cilium
 NGINX_IMAGE=public.ecr.aws/docker/library/nginx
 ARCH=$(uname -m)
 
@@ -26,9 +23,6 @@ then
     echo "git cloning failed, please try again."
     exit
   fi
-  # cd kubespray
-  # sudo git checkout -b release-2.28 remotes/origin/release-2.28 
-  # cd ..
 fi
 
 apt-get update -q \
@@ -58,7 +52,7 @@ echo 'etcd_memory_limit: "8GB"' >> roles/kubernetes/control-plane/defaults/main/
 ansible-playbook -i ${CONFIG_FILE} cluster.yml --become-user=root  \
   -e kube_version=${KUBE_VERSION} -e kube_pods_subnet=${KUBE_PODS_SUBNET} -e kube_service_addresses=${KUBE_SERVICE_ADDRESSES}\
   -e nodelocaldns_ip=${NODE_LOCAL_DNS_IP} -e auto_renew_certificates=true \
-  -e nginx_image_repo=$NGINX_IMAGE -e kube_network_plugin=cilium -b -vvv
+  -e nginx_image_repo=${NGINX_IMAGE} -e kube_network_plugin=${KUBEENETWORKPLUGIN} -b -vvv
 
 if [ ! -f $KUBECONFIG ]
 then
@@ -79,42 +73,24 @@ then
 fi
 
 helm repo add rocm https://rocm.github.io/gpu-operator
+helm upgrade --install amd-gpu-operator -n kube-amd-gpu rocm/gpu-operator-charts --create-namespace
 
-helm repo add exporter https://rocm.github.io/device-metrics-exporter
-helm repo add grafana https://grafana.github.io/helm-charts
-helm repo add vm https://victoriametrics.github.io/helm-charts/
-helm repo add coreweave https://charts.coreweave.com
-helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm install prometheus prometheus-community/kube-prometheus-stack \ 
+helm upgrade --install prometheus prometheus-community/kube-prometheus-stack \ 
       -n prometheus --create-namespace --set installCRDs=true
 
-helm repo add metrics-server https://kubernetes-sigs.github.io/metrics-server/
-
-# helm repo add rook-release https://charts.rook.io/release
-# helm repo add ceph-csi https://ceph.github.io/csi-charts
-# helm repo add pingcap https://charts.pingcap.org/
-# helm repo add juicefs https://juicedata.github.io/charts/
-
-
-helm install amd-gpu-operator -n kube-amd-gpu rocm/gpu-operator-charts --create-namespace
-# helm install vmoperator -n monitoring vm/victoria-metrics-operator --create-namespace
-# helm install tidb-operator -n=tidb-admin pingcap/tidb-operator --version=v1.6.1 --create-namespace 
 
 helm repo add jetstack https://charts.jetstack.io --force-update
-
-helm install \
-    cert-manager jetstack/cert-manager \
-    --namespace cert-manager \
-    --create-namespace \
-    --version v1.17.2 \
-    --set crds.enabled=true
+helm install cert-manager jetstack/cert-manager --namespace cert-manager \
+    --create-namespace --version v1.17.2 --set crds.enabled=true
 
 helm repo add higress.io https://higress.io/helm-charts
-helm install higress higress.io/higress -n higress-system --create-namespace --set higress-core.gateway.hostNetwork=true
+helm upgrade --install higress higress.io/higress -n higress-system --create-namespace \
+      --set higress-core.gateway.hostNetwork=true
 
 helm repo add nvidia https://helm.ngc.nvidia.com/nvidia
-helm install network-operator nvidia/network-operator -n network-operator --create-namespace
+helm upgrade --install network-operator nvidia/network-operator -n network-operator --create-namespace
 
-helm install primus-safe oci://registry-1.docker.io/primussafe/primus-safe --version 0.2.0 -n primus-safe --create-namespace
+helm upgrade --install primus-safe oci://registry-1.docker.io/primussafe/primus-safe -n primus-safe \
+      --version 0.2.0  --create-namespace
 
