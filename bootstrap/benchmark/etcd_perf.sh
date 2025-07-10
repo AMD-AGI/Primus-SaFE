b27ad99e26c4b9740fb4f8e8ddcf302c04022d68
zhanglei-amd <lzhang24@amd.com>
2025-07-10 10:02:04 +0800

    remove bootstrap (#39)

--- bootstrap/benchmark/etcd_perf.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-hostname=$(hostname)
-
-ETCDCTL_API=3 etcdctl \
-  --endpoints=https://127.0.0.1:2379 \
-  --cacert=/etc/ssl/etcd/ssl/ca.pem \
-  --cert=/etc/ssl/etcd/ssl/node-${hostname}.pem \
-  --key=/etc/ssl/etcd/ssl/node-${hostname}-key.pem check perf \
