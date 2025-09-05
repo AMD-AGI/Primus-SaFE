b4dbf61136bd693bddfb758e65ff2b41bf819f8b
lei.wei <Lei.Wei@amd.com>
2025-09-05 14:08:06 +0800

    add ingress config

--- node-agent/pkg/monitors/scripts/gpu_driver_007.sh
+++ node-agent/pkg/monitors/scripts/gpu_driver_007.sh
@@ -9,8 +9,14 @@ if [ ! -f "/tmp/rocm-smi" ]; then
   exit 0
 fi
 
-EXPECT_MAJOR=$1
-EXPECT_MINOR=$2
+if [ "$#" -lt 1 ]; then
+  echo "Usage: $0 <driver-version>"
+  echo "Example: $0 '6.12.12'"
+  exit 2
+fi
+
+IFS='.' read -ra parts <<< "$1"
+length=${#parts[@]}
 
 version=`nsenter --target 1 --mount --uts --ipc --net --pid -- /usr/bin/rocm-smi --showdriverversion |grep "^Driver version:"`
 if [ $? -ne 0 ]; then
@@ -20,12 +26,16 @@ fi
 major_version=$(echo "$version" | cut -d ' ' -f 3 | cut -d '.' -f 1)
 minor_version=$(echo "$version" | cut -d ' ' -f 3 | cut -d '.' -f 2)
 
-if [ -n "$EXPECT_MAJOR" ] && [ "$major_version" != "$EXPECT_MAJOR" ]; then
-  echo "current gpu driver major version is $major_version, but the expect value is $EXPECT_MAJOR"
-  exit 1
+if [ $length -ge 1 ]; then
+  if [ -n "${parts[0]}" ] && [ "$major_version" != "${parts[0]}" ]; then
+    echo "current gpu driver major version is $major_version, but the expect value is ${parts[0]}"
+    exit 1
+  fi
 fi
 
-if [ -n "$EXPECT_MINOR" ] && [ "$minor_version" != "$EXPECT_MINOR" ]; then
-  echo "current gpu driver minor version is $minor_version, but the expect value is $EXPECT_MINOR"
-  exit 1
+if [ $length -ge 2 ]; then
+  if [ -n "${parts[1]}" ] && [ "$minor_version" != "${parts[1]}" ]; then
+    echo "current gpu driver minor version is $minor_version, but the expect value is ${parts[1]}"
+    exit 1
+  fi
 fi
