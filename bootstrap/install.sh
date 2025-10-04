f672cdd1704d136dbefe60482c0a51280179e41e
lei.wei <Lei.Wei@amd.com>
2025-10-04 12:28:42 +0800

    upgrade copyright

--- bootstrap/install.sh
+++ bootstrap/install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+#
+# Copyright (C) 2025-2025, Advanced Micro Devices, Inc. All rights reserved.
+# See LICENSE for license information.
+#
+
 set -euo pipefail
 if ! command -v helm &> /dev/null; then
   echo "Error: helm command not found. Please install it first."
--- charts/hack/role.sh
+++ charts/hack/role.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+
 #
-# Copyright (c) 2025, Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2025-2025, Advanced Micro Devices, Inc. All rights reserved.
 # See LICENSE for license information.
 #
+
 role_file="primus-safe/templates/rbac/role.yaml"
 
 if [ -f ${role_file} ]; then
