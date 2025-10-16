5075f9e250ec045ad6442d02b511199f567ef770
xiaofei.zheng <xiaofei.zheng@amd.com>
2025-10-16 10:29:18 +0800

    feat: log

--- apiserver/pkg/handlers/ssh-handlers/ssh_test.go
+++ apiserver/pkg/handlers/ssh-handlers/ssh_test.go
@@ -12,8 +12,6 @@ import (
 	"gotest.tools/assert"
 )
 
-// ws://149.28.117.31:32736/api/v1/workloads/test-ssh-8kc7b/pods/test-ssh-8kc7b-master-0/webshell?namespace=safe-cluster-dev&rows=1800&cols=40&container=pytorch&cmd=bash
-// ssh 6893ea02fd55c76ec4bc2ff8136f39f4.test-ssh-8kc7b-master-0.pytorch.bash.safe-cluster-dev@149.28.117.31 -p 31748
 func TestParseSSHInfo(t *testing.T) {
 	info := &UserInfo{}
 	user := "root.primus-test-master-0.main.bash.primus-safe-dev"
