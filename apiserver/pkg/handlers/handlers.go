b7414c3c7338cfe616d5afed4d5c53ac1f8aaee8
lei.wei <Lei.Wei@amd.com>
2025-10-17 17:18:23 +0800

    remove CorsMiddleware

--- apiserver/pkg/handlers/handlers.go
+++ apiserver/pkg/handlers/handlers.go
@@ -7,18 +7,15 @@ package handlers
 
 import (
 	"context"
-	"net/http"
 
 	"github.com/gin-gonic/gin"
 	ctrlruntime "sigs.k8s.io/controller-runtime"
 
-	v1 "github.com/AMD-AIG-AIMA/SAFE/apis/pkg/apis/amd/v1"
 	customhandler "github.com/AMD-AIG-AIMA/SAFE/apiserver/pkg/handlers/custom-handlers"
 	image_handlers "github.com/AMD-AIG-AIMA/SAFE/apiserver/pkg/handlers/image-handlers"
 	sshhandler "github.com/AMD-AIG-AIMA/SAFE/apiserver/pkg/handlers/ssh-handlers"
 	apiutils "github.com/AMD-AIG-AIMA/SAFE/apiserver/pkg/utils"
 	commonerrors "github.com/AMD-AIG-AIMA/SAFE/common/pkg/errors"
-	"github.com/AMD-AIG-AIMA/SAFE/utils/pkg/netutil"
 )
 
 // InitHttpHandlers: initializes the HTTP handlers for the API server.
@@ -27,7 +24,7 @@ import (
 // Returns the configured Gin engine or an error if initialization fails.
 func InitHttpHandlers(_ context.Context, mgr ctrlruntime.Manager) (*gin.Engine, error) {
 	engine := gin.New()
-	engine.Use(apiutils.Logger(), gin.Recovery(), CorsMiddleware())
+	engine.Use(apiutils.Logger(), gin.Recovery())
 	engine.NoRoute(func(c *gin.Context) {
 		apiutils.AbortWithApiError(c, commonerrors.NewNotFoundWithMessage(c.Request.RequestURI+" not found"))
 	})
@@ -55,31 +52,4 @@ func InitHttpHandlers(_ context.Context, mgr ctrlruntime.Manager) (*gin.Engine, 
 // Returns the SSH handler or an error if initialization fails.
 func InitSshHandlers(ctx context.Context, mgr ctrlruntime.Manager) (*sshhandler.SshHandler, error) {
 	return sshhandler.NewSshHandler(ctx, mgr)
-}
-
-// CorsMiddleware: provides Cross-Origin Resource Sharing (CORS) support for the API.
-// It sets appropriate CORS headers based on the request origin.
-// For OPTIONS requests, it returns an error with httpcode 204
-func CorsMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		origin := c.GetHeader("Origin")
-		if origin == "" {
-			referer := c.GetHeader("Referer")
-			if referer != "" {
-				origin = netutil.GetSchemeHost(referer)
-			}
-		}
-		if origin != "" && origin != "*" {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-			c.Writer.Header().Set("Access-Control-Allow-Credentials", v1.TrueStr)
-		}
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, "+
-			"Content-Length, Authorization, Accept, X-Requested-With")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS")
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-		c.Next()
-	}
 }
