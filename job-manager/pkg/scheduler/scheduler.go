2451dcf7a3af19ce3cbc363a70e299e8dc4dc3c2
xiaofei.zheng <xiaofei.zheng@amd.com>
2025-10-21 19:09:42 +0800

    feature: scheduler add watch

--- job-manager/pkg/scheduler/scheduler.go
+++ job-manager/pkg/scheduler/scheduler.go
@@ -16,6 +16,8 @@ import (
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/util/workqueue"
 	"k8s.io/klog/v2"
 	"k8s.io/utils/pointer"
 	ctrlruntime "sigs.k8s.io/controller-runtime"
@@ -26,6 +28,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	v1 "github.com/AMD-AIG-AIMA/SAFE/apis/pkg/apis/amd/v1"
 	"github.com/AMD-AIG-AIMA/SAFE/common/pkg/common"
@@ -71,6 +74,7 @@ func SetupSchedulerController(ctx context.Context, mgr manager.Manager) error {
 		For(&v1.Workload{}, builder.WithPredicates(predicate.Or(
 			r.relevantChangePredicate(), predicate.GenerationChangedPredicate{}))).
 		Watches(&v1.Workspace{}, r.handleWorkspaceEvent()).
+		Watches(&v1.Workload{}, r.enqueueDependents(), builder.WithPredicates(r.caredFlowChangePredicate())).
 		Complete(r)
 	if err != nil {
 		return err
@@ -95,6 +99,23 @@ func (r *SchedulerReconciler) relevantChangePredicate() predicate.Predicate {
 				return true
 			}
 			if len(oldWorkload.Status.DependenciesPhase) != len(newWorkload.Status.DependenciesPhase) {
+				return true
+			}
+			return false
+		},
+	}
+}
+
+// caredFlowChangePredicate defines which Workspace changes should trigger scheduling reconciliation.
+func (r *SchedulerReconciler) caredFlowChangePredicate() predicate.Predicate {
+	return predicate.Funcs{
+		UpdateFunc: func(e event.UpdateEvent) bool {
+			oldWorkload, ok1 := e.ObjectOld.(*v1.Workload)
+			newWorkload, ok2 := e.ObjectNew.(*v1.Workload)
+			if !ok1 || !ok2 {
+				return false
+			}
+			if !oldWorkload.IsEnd() && newWorkload.IsEnd() {
 				return true
 			}
 			return false
@@ -773,4 +794,58 @@ func (r *SchedulerReconciler) updateUnScheduled(ctx context.Context, workloads [
 		}
 		order++
 	}
+}
+
+// enqueueDependents creates an event handler that watches for changes in Workload resources.
+func (r *SchedulerReconciler) enqueueDependents() handler.EventHandler {
+	return handler.Funcs{
+		UpdateFunc: func(ctx context.Context, e event.TypedUpdateEvent[client.Object], w workqueue.TypedRateLimitingInterface[reconcile.Request]) {
+			oldWorkload, ok1 := e.ObjectOld.(*v1.Workload)
+			newWorkload, ok2 := e.ObjectNew.(*v1.Workload)
+			if !ok1 || !ok2 {
+				return
+			}
+			if !oldWorkload.IsEnd() && newWorkload.IsEnd() {
+				var dependents v1.WorkloadList
+				if err := r.List(ctx, &dependents, client.MatchingFields{"spec.dependencies": newWorkload.Name}); err != nil {
+					klog.Errorf("failed to list dependencies for workload %s: %v", newWorkload.Name, err)
+					return
+				}
+				for _, dep := range dependents.Items {
+					isAddQueue, err := r.setDependenciesPhase(ctx, newWorkload, dep.Name)
+					if err != nil {
+						klog.Errorf("failed to set dependency phase for workload %s in dependent workload %s: %v", newWorkload.Name, dep.Name, err)
+						continue
+					}
+					if isAddQueue {
+						depName := reconcile.Request{NamespacedName: types.NamespacedName{Name: dep.Name}}
+						klog.Infof("enqueue dependent workload %s of workload %s", depName, newWorkload.Name)
+						w.Add(depName)
+					}
+				}
+				return
+			}
+		},
+	}
+}
+
+// setDependenciesPhase sets the phase of a dependent workload based on the status of its dependency.
+func (r *SchedulerReconciler) setDependenciesPhase(ctx context.Context, workload *v1.Workload, depWorkloadId string) (bool, error) {
+	depWorkload := new(v1.Workload)
+	if err := r.Get(context.Background(), types.NamespacedName{Name: depWorkloadId}, depWorkload); err != nil {
+		return true, err
+	}
+	depWorkload.SetDependenciesPhase(workload.Name, workload.Status.Phase)
+	if workload.Status.Phase != v1.WorkloadSucceeded {
+		if err := jobutils.SetWorkloadFailed(ctx, r.Client, depWorkload, fmt.Sprintf("dependency workload %s failed", workload.Name)); err != nil {
+			return true, err
+		}
+		return false, nil
+	}
+
+	if err := r.Status().Update(context.Background(), depWorkload); err != nil {
+		return true, err
+	}
+
+	return true, nil
 }
