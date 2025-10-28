0a391f415263e37b59a2733a4ee5ee756399f022
ulyssesk <ulyssesk718@outlook.com>
2025-10-28 15:27:45 +0800

    bugfix

--- common/pkg/database/client/notification.go
+++ common/pkg/database/client/notification.go
@@ -2,17 +2,21 @@ package client
 
 import (
 	"context"
+	"errors"
 	"time"
 
 	"github.com/AMD-AIG-AIMA/SAFE/common/pkg/database/client/dal"
 	"github.com/AMD-AIG-AIMA/SAFE/common/pkg/database/client/model"
+	"gorm.io/gorm"
 )
 
 func (c *Client) SubmitNotification(ctx context.Context, data *model.Notification) error {
 	q := dal.Use(c.gorm).Notification
 	existNotification, err := q.WithContext(ctx).Where(q.UID.Eq(data.UID), q.Topic.Eq(data.Topic)).First()
 	if err != nil {
-		return err
+		if !errors.Is(err, gorm.ErrRecordNotFound) {
+			return err
+		}
 	}
 	if existNotification != nil {
 		// Notification already exists
