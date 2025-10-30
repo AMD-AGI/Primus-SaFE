779b3bf6ba9177d6dda69255fe6f1440b96d975e
zhanglei-amd <lzhang24@amd.com>
2025-10-30 14:54:31 +0800

    remove bench (#104)

--- bench/preflight/node/install/install_linux_tools.sh
+++
@@ -1,8 +0,0 @@
-linux_tools="linux-tools-$(uname -r)"
-dpkg -l | grep -q "$linux_tools"
-if [ $? -ne 0 ]; then
-  apt-get update >/dev/null && apt install -y "$linux_tools"  linux-tools-common linux-tools-6.8.0-60-generic linux-cloud-tools-6.8.0-60-generic >/dev/null
-  if [ $? -ne 0 ]; then
-    exit 1
-  fi
-fi
--- bench/scripts/replace_window_r.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find . -type f -name "*.sh" | while read file; do
-    echo "$file"
-    sed -i 's/\r$//' "$file"
-done
-
-echo "Completed"
