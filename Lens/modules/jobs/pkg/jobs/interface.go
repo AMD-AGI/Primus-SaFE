d0741db7b22ceb8c3a09622ce81db30cb0806495
haiskong <haishuo.kong@amd.com>
2025-11-05 17:20:50 +0800

    jobs launch bugfix

--- Lens/modules/jobs/pkg/jobs/interface.go
+++ Lens/modules/jobs/pkg/jobs/interface.go
@@ -24,19 +24,23 @@ type Job interface {
 	Schedule() string
 }
 
-var jobs = []Job{
-	&gpu_allocation.GpuAllocationJob{},
-	&gpu_consumers.GpuConsumersJob{},
-	&node_info.NodeInfoJob{},
-	&device_info.DeviceInfoJob{},
-	&gpu_workload.GpuWorkloadJob{},
-	&gpu_pod.GpuPodJob{},
-	&storage_scan.StorageScanJob{},
-	&cluster_overview.ClusterOverviewJob{},
-	// GPU cache jobs - split into separate jobs for better performance
-	&gpu_realtime_cache.GpuRealtimeCacheJob{},      // Every 30s - realtime metrics
-	&gpu_history_cache_1h.GpuHistoryCache1hJob{},   // Every 1m - 1 hour history
-	&gpu_history_cache_6h.GpuHistoryCache6hJob{},   // Every 5m - 6 hour history
-	&gpu_history_cache_24h.GpuHistoryCache24hJob{}, // Every 10m - 24 hour history
-	gpu_aggregation.NewGpuAggregationJob(),
+var jobs = []Job{}
+
+func InitJobs() {
+	jobs = []Job{
+		&gpu_allocation.GpuAllocationJob{},
+		&gpu_consumers.GpuConsumersJob{},
+		&node_info.NodeInfoJob{},
+		&device_info.DeviceInfoJob{},
+		&gpu_workload.GpuWorkloadJob{},
+		&gpu_pod.GpuPodJob{},
+		&storage_scan.StorageScanJob{},
+		&cluster_overview.ClusterOverviewJob{},
+		// GPU cache jobs - split into separate jobs for better performance
+		&gpu_realtime_cache.GpuRealtimeCacheJob{},      // Every 30s - realtime metrics
+		&gpu_history_cache_1h.GpuHistoryCache1hJob{},   // Every 1m - 1 hour history
+		&gpu_history_cache_6h.GpuHistoryCache6hJob{},   // Every 5m - 6 hour history
+		&gpu_history_cache_24h.GpuHistoryCache24hJob{}, // Every 10m - 24 hour history
+		gpu_aggregation.NewGpuAggregationJob(),
+	}
 }
--- Lens/modules/jobs/pkg/jobs/runner.go
+++ Lens/modules/jobs/pkg/jobs/runner.go
@@ -9,28 +9,29 @@ import (
 )
 
 func Start(ctx context.Context) error {
+	InitJobs()
 	c := cron.New()
 	cm := clientsets.GetClusterManager()
 	currentCluster := cm.GetCurrentClusterClients()
-	
+
 	// Register all jobs with metrics collection
 	for _, job := range jobs {
 		// Capture job variable for closure
 		jobToRun := job
 		jobName := getJobName(jobToRun)
-		
+
 		_, err := c.AddFunc(job.Schedule(), func() {
 			runJobWithMetrics(ctx, jobToRun, currentCluster.K8SClientSet, currentCluster.StorageClientSet)
 		})
-		
+
 		if err != nil {
 			log.Errorf("Failed to schedule job %s: %v", jobName, err)
 			return err
 		}
-		
+
 		log.Infof("Registered job: %s with schedule: %s", jobName, job.Schedule())
 	}
-	
+
 	c.Start()
 	log.Infof("Started %d jobs", len(jobs))
 	return nil
