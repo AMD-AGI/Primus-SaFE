f2cd8c6b84638b81def585b61aec146bb160bd0f
haiskong <haishuo.kong@amd.com>
2025-11-11 10:46:54 +0800

    card second to card hr

--- Lens/modules/core/pkg/helper/workload/workload.go
+++ Lens/modules/core/pkg/helper/workload/workload.go
@@ -99,6 +99,10 @@ func GetWorkloadResource(ctx context.Context, clusterName string, workloadUid st
 		}
 		result[podResource.GpuModel] += endTime.Sub(podResource.CreatedAt).Seconds() * float64(podResource.GpuAllocated)
 	}
+	// 将秒转换为小时
+	for gpuModel := range result {
+		result[gpuModel] = result[gpuModel] / 3600
+	}
 	return result, nil
 }
 
