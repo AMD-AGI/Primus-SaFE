adb8c66f8ffe8ec8fa8248823e37a9f5b29aa071
weilei0120 <lei.wei@amd.com>
2025-11-19 08:24:58 +0800

    Feature/weilei/support install openssh (#164)
    * add WORKLOAD_KIND to Environments
    
    * add build-ssh in launcher.sh
    
    * add preprocess workflow for github

--- SaFE/job-manager/installer/preprocess/launcher.sh
+++ SaFE/job-manager/installer/preprocess/launcher.sh
@@ -9,22 +9,11 @@ input="$1"
 
 export NODE_RANK="${PET_NODE_RANK:-${NODE_RANK}}"
 export NNODES="${PET_NNODES:-${NNODES}}"
+export PATH_TO_BNXT_TAR_PACKAGE=$PATH_TO_BNXT_TAR_PACKAGE
+export WORKLOAD_KIND=$WORKLOAD_KIND
 
-if [ -f "${PATH_TO_BNXT_TAR_PACKAGE}" ]; then
-  echo "Rebuild bnxt from $PATH_TO_BNXT_TAR_PACKAGE ..." && \
-  tar xzf "${PATH_TO_BNXT_TAR_PACKAGE}" -C /tmp/ && \
-  mv /tmp/libbnxt_re-* /tmp/libbnxt && \
-  mv /usr/lib/x86_64-linux-gnu/libibverbs/libbnxt_re-rdmav34.so /usr/lib/x86_64-linux-gnu/libibverbs/libbnxt_re-rdmav34.so.inbox && \
-  cd /tmp/libbnxt/ && sh ./autogen.sh && ./configure && \
-  make -C /tmp/libbnxt clean all install && \
-  echo '/usr/local/lib' > /etc/ld.so.conf.d/libbnxt_re.conf && \
-  ldconfig && \
-  cp -f /tmp/libbnxt/bnxt_re.driver /etc/libibverbs.d/ && \
-  cd "${PRIMUS_PATH}" && \
-  echo "Rebuild libbnxt done."
-else
-  echo "Skip bnxt rebuild. PATH_TO_BNXT_TAR_PACKAGE=$PATH_TO_BNXT_TAR_PACKAGE"
-fi
+sh /shared-data/build_bnxt.sh
+sh /shared-data/build_ssh.sh
 
 echo "$input" |base64 -d > ".run.sh"
 chmod +x ".run.sh"
