d022a6aea3631cde9c4e979f06ad4b89306f6b29
weilei0120 <lei.wei@amd.com>
2025-12-02 11:52:12 +0800

    add detail reason (#191)

--- SaFE/job-manager/pkg/scheduler/scheduler_utils.go
+++ SaFE/job-manager/pkg/scheduler/scheduler_utils.go
@@ -92,6 +92,9 @@ func buildReason(workload *v1.Workload, podResources corev1.ResourceList, nodes 
 	reason := ""
 	if len(nodes) == 0 {
 		reason = "All nodes are unavailable"
+		if len(workload.Spec.CustomerLabels) > 0 {
+			reason += " or not enough nodes match the specified label."
+		}
 	} else {
 		sort.Slice(nodes, func(i, j int) bool {
 			if floatutil.FloatEqual(nodes[i].resourceScore, nodes[j].resourceScore) {
@@ -100,10 +103,8 @@ func buildReason(workload *v1.Workload, podResources corev1.ResourceList, nodes 
 			return nodes[i].resourceScore > nodes[j].resourceScore
 		})
 		_, key := quantity.IsSubResource(podResources, nodes[0].resource)
-		reason = fmt.Sprintf("Insufficient %s due to node fragmentation", formatResourceName(key))
-	}
-	if len(workload.Spec.CustomerLabels) > 0 {
-		reason += " or not enough nodes match the specified label."
+		reason = fmt.Sprintf("Insufficient %s on %s due to node fragmentation",
+			formatResourceName(key), nodes[0].node.Name)
 	}
 	return reason
 }
