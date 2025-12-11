bd20c1e02c1860210c7b97aa0b49c38b25f740e2
leiwei12 <lei.wei@amd.com>
2025-12-11 14:07:16 +0800

    support job kind

--- SaFE/common/pkg/workload/workload.go
+++ SaFE/common/pkg/workload/workload.go
@@ -199,7 +199,7 @@ func GetPodResources(res *v1.WorkloadResource) (corev1.ResourceList, error) {
 // GetScope determines the workspace scope based on workload kind.
 func GetScope(w *v1.Workload) v1.WorkspaceScope {
 	switch w.SpecKind() {
-	case common.PytorchJobKind, common.UnifiedJobKind:
+	case common.PytorchJobKind, common.UnifiedJobKind, common.JobKind:
 		return v1.TrainScope
 	case common.DeploymentKind, common.StatefulSetKind:
 		return v1.InferScope
