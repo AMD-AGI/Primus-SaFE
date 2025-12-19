0949a4434a0600db48b8f6866ac7f4cb8c5c4c0e
leiwei12 <lei.wei@amd.com>
2025-12-19 16:46:23 +0800

    support ainic check

---
+++ SaFE/node-agent/pkg/monitors/scripts/net_ainic_load_205.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# Copyright (C) 2025-2025, Advanced Micro Devices, Inc. All rights reserved.
+# See LICENSE for license information.
+#
+
+set -o pipefail
+
+nsenter --target 1 --mount --uts --ipc --net --pid -- lsmod |grep '^ionic_rdma ' > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Error: unable to find ionic_rdma module"
+  exit 1
+fi
+
+nsenter --target 1 --mount --uts --ipc --net --pid -- lsmod |grep '^ionic ' > /dev/null
+if [ $? -ne 0 ]; then
+  echo "Error: unable to find ionic module"
+  exit 1
+fi
