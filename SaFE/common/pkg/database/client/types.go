e658458ed47c93c4a3ef7d58b3a75559b31ae83c
lishuoshuo-amd <shuoli@amd.com>
2026-01-08 11:44:03 +0800

    feat: apikey handle (#276)
    * feat: apikey handle
    
    * feature: apikey
    
    * feat: add keyHint
    
    * feat: fix key hint
    
    * fix
    
    * fix whitelist validate
    
    * fix: Modify the issue of duplicate IP address resolution

--- SaFE/common/pkg/database/client/types.go
+++ SaFE/common/pkg/database/client/types.go
@@ -318,3 +318,24 @@ func GetEnvironmentSnapshotFieldTags() map[string]string {
 	e := EnvironmentSnapshot{}
 	return getFieldTags(e)
 }
+
+// ApiKey represents an API key record in the database
+type ApiKey struct {
+	Id             int64       `db:"id"`
+	Name           string      `db:"name"`
+	UserId         string      `db:"user_id"`
+	UserName       string      `db:"user_name"`
+	ApiKey         string      `db:"api_key"`
+	KeyHint        string      `db:"key_hint"` // Partial key for display: "XX-YYYY" (first 2 + last 4 chars after prefix)
+	ExpirationTime pq.NullTime `db:"expiration_time"`
+	CreationTime   pq.NullTime `db:"creation_time"`
+	Whitelist      string      `db:"whitelist"` // JSON string of IP/CIDR list
+	Deleted        bool        `db:"deleted"`
+	DeletionTime   pq.NullTime `db:"deletion_time"`
+}
+
+// GetApiKeyFieldTags returns the ApiKeyFieldTags value.
+func GetApiKeyFieldTags() map[string]string {
+	k := ApiKey{}
+	return getFieldTags(k)
+}
