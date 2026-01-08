fc483c7a70364e18c52af89585f0744918823e8d
haishuok0525 <haiskong@amd.com>
2026-01-08 19:18:43 +0800

    Add license prehook and license header (#281)
    * test: verify pre-commit hook adds license header
    
    * chore(lens): add license headers to all Go and Python files
    
    - Add license header templates (license-go.tpl, license-py.tpl)
    - Add Makefile with license management targets
    - Add pre-commit hook to auto-add license headers
    - Apply license headers to all existing Go and Python files

--- Lens/modules/core/pkg/model/reuse_config.go
+++ Lens/modules/core/pkg/model/reuse_config.go
@@ -1,3 +1,6 @@
+// Copyright (C) 2025-2026, Advanced Micro Devices, Inc. All rights reserved.
+// See LICENSE for license information.
+
 package model
 
 // ReuseConfig represents the configuration for the reuse engine
