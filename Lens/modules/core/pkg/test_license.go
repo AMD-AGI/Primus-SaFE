f31546a626a2b8a2dfb271900ac65cd5ef9356c0
haiskong <haishuo.kong@amd.com>
2026-01-08 16:30:02 +0800

    test: verify pre-commit hook adds license header

---
+++ Lens/modules/core/pkg/test_license.go
@@ -0,0 +1,9 @@
+// Copyright (C) 2025-2026, Advanced Micro Devices, Inc. All rights reserved.
+// See LICENSE for license information.
+
+package pkg
+
+// This is a test file to verify pre-commit hook adds license header
+func TestLicenseHook() string {
+	return "hello"
+}
