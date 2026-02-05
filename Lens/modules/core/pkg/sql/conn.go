d4ba633941f87f23b1a03779c17d2faaec29b510
haiskong <haiskong@amd.com>
2026-02-05 12:08:30 +0000

    Revert "fix(core): register control plane DB as default for BaseFacade access"
    This reverts commit 96d89b782100219d51423038075357f11b23af74.

--- Lens/modules/core/pkg/sql/conn.go
+++ Lens/modules/core/pkg/sql/conn.go
@@ -148,14 +148,3 @@ func GetDB(key string) *gorm.DB {
 func GetDefaultDB() *gorm.DB {
 	return GetDB(dbKeyDefault)
 }
-
-// RegisterDB registers an existing database connection with a key
-// This allows sharing a single connection under multiple keys
-func RegisterDB(key string, db *gorm.DB) {
-	if db == nil {
-		return
-	}
-	connPoolLock.Lock()
-	defer connPoolLock.Unlock()
-	connPools[key] = db
-}
