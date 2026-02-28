c16cca54bbcf2bebd8a2bd6475ef73866026170c
leiwei12 <lei.wei@amd.com>
2026-02-28 17:44:49 +0800

    remove sysctl log

--- Bench/addons/scripts/sysctl_system.sh
+++ Bench/addons/scripts/sysctl_system.sh
@@ -3,4 +3,5 @@
 echo "fs.inotify.max_user_instances=1024" > /etc/sysctl.d/99-inotify.conf
 echo "fs.inotify.max_user_watches=1048576" >> /etc/sysctl.d/99-inotify.conf
 echo "fs.inotify.max_queued_events=16384" >> /etc/sysctl.d/99-inotify.conf
-sysctl --system
+echo "vm.max_map_count=262144" > /etc/sysctl.d/99-max-map-count.conf
+sysctl --system >/dev/null 2>&1
